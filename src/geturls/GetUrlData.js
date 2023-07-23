0debf67896c22057e33cb4fadff78d118360b8fd
Avinash <avibhardwaj1410@gmail.com>
2023-07-23 11:13:23 +0530

    frontend  q1

---
+++ src/geturls/GetUrlData.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const GetUrlData = () => {
+  const [urls, setUrls] = useState([]);
+  const [dataResponses, setDataResponses] = useState([]);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const urlValues = urlParams.getAll('url');
+    setUrls(urlValues);
+
+    const fetchData = async () => {
+      try {
+        const responses = await Promise.all(
+          urls.map((url) => axios.get(url))
+        );
+        setDataResponses(
+          responses.map((response) =>
+            response.data.numbers.sort((a, b) => a - b).filter((value, index, self) => self.indexOf(value) === index)
+          )
+        );
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [urls]);
+
+  return (
+    <div>
+      <h2>Data from URLs:</h2>
+      <ul>
+        {dataResponses.map((numbers, index) => (
+          <li key={index}>{JSON.stringify(numbers)}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default GetUrlData;
